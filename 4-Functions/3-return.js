101007419deee96a581ce578447a641b2ec3a324
Captain Storm <CaptainStorm21@users.noreply.github.com>
2020-01-15 17:08:02 -0800

    Create 3-return.js

---
+++ 4-Functions/3-return.js
@@ -0,0 +1,35 @@
+//returns undefined
+function greeting(){
+  console.log('hello')
+}
+
+console.log(greeting);
+greeting();
+
+
+//return a value
+function sum(a, b){
+  return a+b;
+}
+
+var x = sum (9, 8);
+console.log(x);
+
+
+// return a function
+function greet() {
+  return function () {
+    console.log('Good morning')
+  }
+}
+greet()();
+
+
+
+// return a function 2
+function greet2(x) {
+  return function (name) {
+    console.log('Good night, ' + name)
+  }
+}
+greet2()('Martha')
