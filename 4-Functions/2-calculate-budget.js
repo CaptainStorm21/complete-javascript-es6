c953a377515b5175fd60cdf88621d097c8ac9a90
Captain Storm <CaptainStorm21@users.noreply.github.com>
2020-01-15 16:41:08 -0800

    Update 2-calculate-budget.js

--- 4-Functions/2-calculate-budget.js
+++ 4-Functions/2-calculate-budget.js
@@ -1,3 +1,4 @@
+
 /*
 calculateCost function params
 hotelPrice
@@ -12,10 +13,8 @@ write a function which takes an input
 - duration days stay
 - flight cost
 - budge avail 
-
 prints the total cost of the plan and return a string 
 "lets' go!" or "change your plan"
-
 */
 
 //build the calculateCost
@@ -23,7 +22,15 @@ function calculateCost (hotelPrice, duration, ticketPrice, budget){
     var totalCost = hotelPrice * duration + ticketPrice;
     console.log("The total cost is $" + totalCost);
     console.log("your budge is $" + budget);
+  
+  //decide on when when you are within the budget or not
+  var result;
+  if(totalCost <= budget){
+   console.log( "lets' go!");
+  } else {
+   console.log( "Over the budget! Please change your plans.");
+  }
                
 }
 
-calculateCost (50, 10, 300, 900);
+calculateCost (50, 10, 300, 200);
