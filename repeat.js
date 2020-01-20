c494e2f96dc177d25736492f5afef0150914ead2
Captain Storm <CaptainStorm21@users.noreply.github.com>
2020-01-20 12:57:26 -0800

    Create repeat.js

---
+++ repeat.js
@@ -0,0 +1,20 @@
+//old way
+
+const strings = Array(20).fill('*').join('');
+console.log(strings)
+console.log(strings.length)
+
+//repeat a function 
+const es6strings = '*'.repeat(50);
+console.log(es6strings)
+console.log(es6strings.length)
+
+
+/*
+output 
+
+"********************"
+20
+"**************************************************"
+50
+*/
