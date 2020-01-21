52d71f7ae003b619a80b487850da571d7d49077d
Captain Storm <CaptainStorm21@users.noreply.github.com>
2020-01-20 18:13:45 -0800

    Create math-trunc.js

---
+++ math-trunc.js
@@ -0,0 +1,13 @@
+//truncate
+function trunc(x){
+  return (x<0 ? Math.cell(x) : Math.floor(x));
+}
+
+console.log(trunc(42.5555));
+//output 42
+
+
+//es6 Math.trunc
+console.log(Math.trunc(-213.34234));
+//output -213
+
